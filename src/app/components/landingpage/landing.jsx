788e460df5ab890d8938ae948d82e7024f2c6668
Archit <architojha420@gmail.com>
2024-02-15 18:11:10 +0530

    structured the website

---
+++ src/app/components/landingpage/landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./landing.module.css";
+
+const Landing = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}>Check out our Curriculum</div>
+    </div>
+  );
+};
+
+export default Landing;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from './homepage.module.css'
 import { Button } from "@nextui-org/react";
+import Landing from "./components/landingpage/landing";
 
 export default function Home() {
   return (
-    <div className={styles.parentContainer}>
-      
+    <div>
+      <Landing/>
     </div>
   );
 }
